b41f1f6670a3c111b1edcfdce8375be05397bc7c
Suleman <sulemanniazi77@gmail.com>
2026-07-08 17:09:50 +0500

    use built-in fetch

--- api/generate.js
+++ api/generate.js
@@ -1,5 +1,3 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-
 module.exports = async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
   try {
